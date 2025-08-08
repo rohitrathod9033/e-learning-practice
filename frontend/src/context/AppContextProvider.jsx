a51f4a61c0bf921ee6a78c0982f98c125e9e0f64
Rohit Rathod <rohitrathod60371@gmail.com>
2025-08-08 23:58:20 +0530

    Last

--- frontend/src/context/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider.jsx
@@ -42,10 +42,15 @@ export const AppContextProvider = ({ children }) => {
   //Fetch UserData
 
   const fetchUserData = async () => {
+
+    if(user.publicMetadata.role === 'educator') {
+      setIsEducator(true)
+    }
+
     try {
         const token = await getToken();
 
-        const {data} = await axios.get(backendUrl + '/api/user/data',{headers: { Authorization: `Bearer ${token}` }});
+        const {data} = await axios.get(backendUrl + '/api/user/data',{headers:{Authorization: `Bearer ${token}` }});
 
         if (data.success) {
             setUserData(data.user)
@@ -68,7 +73,7 @@ export const AppContextProvider = ({ children }) => {
     course. courseRatings. forEach(rating => {
       totalRating += rating.rating
     })
-    return totalRating / course.courseRatings.length;
+    return Math.floor(totalRating / course.courseRatings.length)
   }
 
   // Function which calculte "chapter time"....
@@ -102,26 +107,33 @@ export const AppContextProvider = ({ children }) => {
 
   // Fetch user enrolled courses...
   const fetchUserEnrolledCourses = async ()=>{
-    setEnrolledCourses(dummyCourses)
+    try {
+      const token = await getToken();
+      const {data} = await axios.get(backendUrl + '/api/user/enrolled-courses', {headers: {Authorization: `Bearer ${token}` }})
+
+      if(data.success) {
+        setEnrolledCourses(data.enrolledCourses.reverse())
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+        toast.error(error.message)
+    }    
 }
   
   useEffect(() => {
     fetchAllCourses();
-    fetchUserEnrolledCourses();
   }, []);
-
-  const logToken = async() => {
-    console.log(await getToken());
-  }
 
   useEffect(() => {
     if(user) {
-      logToken()
+      fetchUserData();
+      fetchUserEnrolledCourses();
     }
   }, [user])
 
   const value = {
-    currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime, enrolledCourses, fetchUserEnrolledCourses
+    currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime, enrolledCourses, fetchUserEnrolledCourses, backendUrl, userData, setUserData, getToken, fetchAllCourses
   };
 
   return (
