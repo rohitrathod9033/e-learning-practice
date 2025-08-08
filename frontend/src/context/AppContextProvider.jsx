b0f25ac50d0051a04d9054ea046d590ca9935c61
Rohit Rathod <rohitrathod60371@gmail.com>
2025-08-08 23:27:53 +0530

    Update

--- frontend/src/context/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider.jsx
@@ -5,8 +5,12 @@ import { AppContext } from "./AppContext";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
 import { useAuth, useUser } from "@clerk/clerk-react"
+import axios from "axios"
+import { toast } from "react-toastify";
 
 export const AppContextProvider = ({ children }) => {
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
 
   const currency = import.meta.env.VITE_CURRENCY || "₹";
   const navigate = useNavigate()
@@ -15,13 +19,45 @@ export const AppContextProvider = ({ children }) => {
   const {user} = useUser()
 
   const [allCourses, setAllCourses] = useState([]);
-  const [isEducator, setIsEducator] = useState([true]); 
-  const [enrolledCourses, setEnrolledCourses] = useState([]); 
+  const [isEducator, setIsEducator] = useState(false); 
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
+  const [userData, setUserData] = useState(null);
 
   // Fetch All Courses 
   const fetchAllCourses = async () => {
-    setAllCourses(dummyCourses);
+    try {
+      const {data} = await axios.get(backendUrl + '/api/course/all')
+
+      if(data.success) {
+        setAllCourses(data.courses)
+      } else {
+         toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
   };
+
+  //Fetch UserData
+
+  const fetchUserData = async () => {
+    try {
+        const token = await getToken();
+
+        const {data} = await axios.get(backendUrl + '/api/user/data',{headers: { Authorization: `Bearer ${token}` }});
+
+        if (data.success) {
+            setUserData(data.user)
+        } else {
+          toast.error(data.message)
+        }
+
+    } catch (error) {
+        toast.error(error.message)
+    }
+}
+
 
   // Function to calculate average rating of course
   const calculateRating = (course)=>{
