91631d2a9adf6c92c6dbd7cf15f44802a9b72d9f
Rohit Rathod <rohitrathod60371@gmail.com>
2025-08-08 22:51:25 +0530

    Push

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import Stripe from "stripe";
 import { Purchase } from "../models/Purchase.js";
 import Course from "../models/Course.js";
+import { CourseProgress } from "../models/CourseProgress.js";
 
 // Get User Data
 export const getUserData = async (req, res) => {
