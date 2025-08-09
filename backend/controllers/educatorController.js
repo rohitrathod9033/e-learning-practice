15f69fbc5ea2e15fca0d8ed6455ca5acc0f64ffe
Rohit Rathod <rohitrathod60371@gmail.com>
2025-08-09 15:35:20 +0530

    Completed

--- backend/controllers/educatorController.js
+++ backend/controllers/educatorController.js
@@ -2,6 +2,7 @@ import { clerkClient } from "@clerk/express";
 import Course from "../models/Course.js"; 
 import { v2 as cloudinary } from "cloudinary";
 import User from "../models/User.js";
+import { Purchase } from "../models/Purchase.js";
 
 // Update Role to Educator.....
 export const updateRoleToEducator = async (req, res) => {
